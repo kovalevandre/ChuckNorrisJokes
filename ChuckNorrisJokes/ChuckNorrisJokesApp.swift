72f0cce501926118f008c7697b9a87b2ece60a0f
Andrey <akovalev@me.com>
2023-12-20 06:55:30 +0300

    Initial Commit

---
+++ ChuckNorrisJokes/ChuckNorrisJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckNorrisJokesApp.swift
+//  ChuckNorrisJokes
+//
+//  Created by Andrey Kovalev on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChuckNorrisJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
